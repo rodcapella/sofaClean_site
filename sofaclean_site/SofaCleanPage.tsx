88fa4dee384bac9d8f00f3555b4a87cc1c2128e0
rodcapella <rodcapella@gmail.com>
2026-09-21 12:42:41 +0100

    update

--- sofaclean_site/SofaCleanPage.tsx
+++ sofaclean_site/SofaCleanPage.tsx
@@ -1,4 +1,11 @@
 'use client';
+import { useState } from 'react';
 import { GeneralServicesLandingPage } from './GeneralServicesLandingPage';
 import { generalServicesContent } from './content';
-export function SofaCleanPage() { return <GeneralServicesLandingPage content={generalServicesContent} demo={false} />; }
+import { ContactModal } from './ContactModal';
+export function SofaCleanPage() {
+  const [contactOpen, setContactOpen] = useState(false);
+  const [topic, setTopic] = useState('');
+  const openContact = (nextTopic = '') => { setTopic(nextTopic); setContactOpen(true); };
+  return <><GeneralServicesLandingPage content={generalServicesContent} demo={false} onOpenContact={openContact} /><ContactModal isOpen={contactOpen} onClose={() => setContactOpen(false)} initialTopic={topic} /></>;
+}
